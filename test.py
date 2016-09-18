efb84b5576a25329f32a609c671a5bfa55bc3855
Coooooder_Y <yangruihan@vip.qq.com>
2016-09-18 15:04:14 +0000

    test.py 进行重构

--- test.py
+++ test.py
@@ -8,32 +8,23 @@ from leptjson import LeptValue, LeptJson
 class LeptTest(unittest.TestCase):
     
     def test_parse_null(self):
-        v, parse_return_code = LeptJson.Parse("null")
-        self.assertEqual(LeptParseReturnValue.ok, parse_return_code)
-        self.assertEqual(LeptType.null, v.type)
+        self.return_value_and_type_test(LeptParseReturnValue.ok, LeptType.null, "null")
         
     def test_parse_expect_value(self):
-        v, parse_return_code = LeptJson.Parse("")
-        self.assertEqual(LeptParseReturnValue.expect_value, parse_return_code)
-        self.assertEqual(LeptType.null, v.type)
-        
-        v, parse_return_code = LeptJson.Parse("  ")
-        self.assertEqual(LeptParseReturnValue.expect_value, parse_return_code)
-        self.assertEqual(LeptType.null, v.type)
+        self.return_value_and_type_test(LeptParseReturnValue.expect_value, LeptType.null, "")
+        self.return_value_and_type_test(LeptParseReturnValue.expect_value, LeptType.null, "  ")
         
     def test_parse_invalid_value(self):
-        v, parse_return_code = LeptJson.Parse("nul")
-        self.assertEqual(LeptParseReturnValue.invalid_value, parse_return_code)
-        self.assertEqual(LeptType.null, v.type)
-        
-        v, parse_return_code = LeptJson.Parse("?")
-        self.assertEqual(LeptParseReturnValue.invalid_value, parse_return_code)
-        self.assertEqual(LeptType.null, v.type)
+        self.return_value_and_type_test(LeptParseReturnValue.invalid_value, LeptType.null, "nul")
+        self.return_value_and_type_test(LeptParseReturnValue.invalid_value, LeptType.null, "?")
         
     def test_parse_root_not_singular(self):
-        v, parse_return_code = LeptJson.Parse("null x")
-        self.assertEqual(LeptParseReturnValue.root_not_singular, parse_return_code)
-        self.assertEqual(LeptType.null, v.type)
+        self.return_value_and_type_test(LeptParseReturnValue.root_not_singular, LeptType.null, "null x")
+        
+    def return_value_and_type_test(self, return_value, type, json):
+        v, parse_return_code = LeptJson.Parse(json)
+        self.assertEqual(return_value, parse_return_code)
+        self.assertEqual(type, v.type)
             
 if __name__ == '__main__':
     unittest.main()
